17bded9e35e189edb76e0e3dfe44870b0478d812
YuriU <jerryhire8@gmail.com>
2019-09-15 21:26:42 +0300

    Attempt to integrate with ApiGateway

--- main.tf
+++ main.tf
@@ -44,7 +44,6 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
-
 resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = "${aws_iam_role.lambda_exec.name}"
   policy_arn = "${module.source_bucket.read_only_policy_arn}"
@@ -57,11 +56,6 @@ resource "aws_iam_role_policy_attachment" "test-attach2" {
 
 resource "aws_lambda_function" "function" {
   function_name = "${var.application_name}"
-
-  # The bucket name as created earlier with "aws s3api create-bucket"
-    # "main" is the filename within the zip file (main.js) and "handler"
-  # is the name of the property under which the handler function was
-  # exported in that file.
   handler       = "personalWebsiteBackend::personalWebsiteBackend.RequestHandler::HandleRequest"
   runtime       = "dotnetcore2.1"
 
@@ -81,4 +75,75 @@ resource "aws_lambda_function" "function" {
       Database_CounterTable = "${module.counter_table.table_name}"
     }
   }
+}
+
+resource "aws_api_gateway_rest_api" "gateway" {
+  name        = "${var.application_name}"
+  description = "Terraform Managed for ${var.application_name}"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
+  path_part   = "Hello"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.function.invoke_arn}"
+}
+
+/*
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.function.invoke_arn}"
+}
+*/
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    //"aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  stage_name  = "default"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.function.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*"
 }
