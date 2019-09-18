8e6877e9c18edbed43d623084213b82226f85048
Yuriy Ulyanets <yuriy.ulyanets@namecheap.com>
2019-09-18 11:43:40 +0300

    Added alias for custom domain

--- main.tf
+++ main.tf
@@ -138,14 +138,29 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*/*"
 }
 
-resource "aws_api_gateway_domain_name" "example" {
-  #depends_on = ["module.certificate"]
-
+resource "aws_api_gateway_domain_name" "gateway_domain" {
   regional_certificate_arn  = "${module.certificate.certificate_arn}"
   domain_name               = "${var.subdomain_name}.${var.domain_name}"
 
   endpoint_configuration {
     types = ["REGIONAL"]
+  }
+}
+
+data "aws_route53_zone" "web_site_zone" {
+  name         = "${var.domain_name}"
+  private_zone = false
+}
+
+resource "aws_route53_record" "gateway_record" {
+  name    = "${aws_api_gateway_domain_name.gateway_domain.domain_name}"
+  type    = "A"
+  zone_id = "${data.aws_route53_zone.web_site_zone.id}"
+
+  alias {
+    evaluate_target_health = true
+    name                   = "${aws_api_gateway_domain_name.gateway_domain.regional_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.gateway_domain.regional_zone_id}"
   }
 }
 
