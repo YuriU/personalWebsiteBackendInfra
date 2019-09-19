a789fc981cfa7670795983c23ef3a081aa3888a1
YuriU <jerryhire8@gmail.com>
2019-09-19 21:06:25 +0300

    Forgotten files

---
+++ modules/api_gateway/main.tf
@@ -0,0 +1,54 @@
+resource "aws_api_gateway_rest_api" "gateway" {
+  name        = "${var.application_name}"
+  description = "Terraform Managed for ${var.application_name}"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  binary_media_types = ["application/msword"]
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
+  path_part   = "${var.path_part}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${var.lambda_invoke_arn}"
+
+  content_handling = "CONVERT_TO_BINARY"
+}
+
+resource "aws_api_gateway_deployment" "api" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  stage_name  = "default"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.lambda_function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.gateway.execution_arn}/*/*/*"
+}
---
+++ modules/api_gateway/variables.tf
@@ -0,0 +1,17 @@
+variable "application_name" {
+  description = "Lambda name"
+}
+
+variable "path_part" {
+  description = "Part of the path"
+}
+
+variable "lambda_invoke_arn" {
+    description = "ARN to the lambda function"
+  
+}
+
+variable "lambda_function_name" {
+    description = "Name of lambda"
+  
+}
